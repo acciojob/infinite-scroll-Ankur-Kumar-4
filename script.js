504b2a358914ce1bd615eb276986cae8b20d9ffb
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-20 11:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const olList = document.getElementById("infi-list");
+  let itemCount = 10; // Initial number of list items
+  const itemsToAdd = 2; // Number of items to add when scrolling
 
+  // Function to add more list items
+  function addMoreItems() {
+    for (let i = 1; i <= itemsToAdd; i++) {
+      const li = document.createElement("li");
+      li.textContent = "Item " + (itemCount + i);
+      olList.appendChild(li);
+    }
+    itemCount += itemsToAdd;
+  }
+
+  // Attach a scroll event listener to the main content area
+  olList.addEventListener("scroll", function () {
+    // Calculate the scroll position
+    const scrollPosition = this.scrollTop + this.clientHeight;
+    const totalHeight = olList.scrollHeight;
+
+    // Check if the user has scrolled to the bottom or near it
+    if (scrollPosition >= totalHeight - this.clientHeight) {
+      // If the user is near the bottom, add more list items
+      addMoreItems();
+    }
+  });
+
+  // Initial loading of list items
+  addMoreItems();
+});
